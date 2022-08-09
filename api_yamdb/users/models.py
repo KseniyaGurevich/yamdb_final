54d1b00a19b4e6d1625cdfcdb9348a14e39b0ac3
Ксения Гуревич <kurvinkl@gmail.com>
2022-08-09 11:33:38 +0300

    build a project

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+CHOICES = (
+    ('user', 'Пользователь'),
+    ('moderator', 'Модератор'),
+    ('admin', 'Администратор'),
+)
+
+
+class User(AbstractUser):
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+    role = models.CharField(default='user', max_length=30, choices=CHOICES)
+    email = models.EmailField(max_length=254, unique=True, blank=False)
